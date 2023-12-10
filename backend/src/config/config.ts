f191f6585797db866d4d45260f50eb1079c83384
Aman Kumar <isharaman8@gmail.com>
2023-12-11 00:38:07 +0530

    fixed upload method

--- backend/src/config/config.ts
+++ backend/src/config/config.ts
@@ -18,6 +18,7 @@ export const config = () => ({
     },
   },
   s3: {
+    region: process.env.S3_REGION,
     bucket: process.env.S3_BUCKET_NAME,
     accessKeyId: process.env.S3_ACCESS_KEY_ID,
     secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
--- backend/src/modules/s3/s3.module.ts
+++ backend/src/modules/s3/s3.module.ts
@@ -1,7 +1,12 @@
+// third party imports
 import { Module } from '@nestjs/common';
+
+// inner imports
 import { S3Service } from './s3.service';
+import { S3Controller } from './s3.controller';
 
 @Module({
+  controllers: [S3Controller],
   providers: [S3Service],
 })
 export class S3Module {}
