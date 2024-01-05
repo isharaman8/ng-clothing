eeb703d4a7fe962b11f427c00e4ee8437c2a2572
Aman Kumar <isharaman8@gmail.com>
2024-01-06 00:50:13 +0530

    fixed navbar

--- frontend/src/components/Navbar/Navbar.ts
+++ frontend/src/components/Navbar/Navbar.ts
@@ -1,3 +1,5 @@
+import _ from 'lodash';
+
 export const getUrl = (code: string): string => {
 	if (code === 'home') {
 		return '/';
@@ -5,3 +7,18 @@ export const getUrl = (code: string): string => {
 
 	return `/${code}`;
 };
+
+// css related
+const getActiveClass = (id: string, section: string) => {
+	if (section === 'home' && id === '/') {
+		return 'active';
+	}
+
+	console.log('ID', id);
+	console.log('SECTION', section);
+	console.log('VALUE', _.includes(id, section) ? 'active' : '');
+
+	return _.includes(id, section) ? 'active' : '';
+};
+
+export const CssMethods = { getActiveClass };
