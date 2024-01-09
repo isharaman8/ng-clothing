f8dcd56535dbc5df78af182287fc0e7b3a1c85ba
Aman Kumar <isharaman8@gmail.com>
2024-01-10 01:18:50 +0530

    added navbar list_product and product_card components

---
+++ frontend/src/components/ListProducts/ListProducts.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	// inner imports
+	import { sampleProducts, sampleTitle } from './ListProducts';
+	import ProductCard from '../ProductCard/ProductCard.svelte';
+
+	export let title = sampleTitle;
+	export let products = sampleProducts;
+</script>
+
+<section class="p-20">
+	<!-- title -->
+	<h1 class="text-3xl font-semibold">{title}</h1>
+
+	<!-- products -->
+	<div class="ml-5 grid grid-cols-4 gap-5 place-items-center place-content-center items-center content-center">
+		{#each products as product}
+			<ProductCard {product} />
+		{/each}
+	</div>
+</section>
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,4 +1,6 @@
 <script>
+	// inner imports
+	import ListProducts from '../components/ListProducts/ListProducts.svelte';
 </script>
 
 <svelte:head>
@@ -6,7 +8,9 @@
 	<meta name="description" content="Svelte demo app" />
 </svelte:head>
 
-<section></section>
+<section class="mt-24 w-[100vw] min-h-[88vh]">
+	<ListProducts title="Trending" />
+</section>
 
 <style>
 </style>
