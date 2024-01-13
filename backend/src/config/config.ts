d554e8d72d26c26de1d86de93409f2b25c061b90
Aman Kumar <isharaman8@gmail.com>
2024-01-13 23:08:51 +0530

    added logs for testing

--- backend/src/config/config.ts
+++ backend/src/config/config.ts
@@ -1,26 +1,32 @@
-export const config = () => ({
-  port: parseInt(process.env.PORT, 10) || 8080,
-  api: {
-    apiUrl: process.env.API_URL,
-    httpTimeout: 1000,
-  },
-  mongodb: {
-    uri: process.env.MONGODB_URI,
-    dbName: process.env.MONGODB_DBNAME,
-    password: process.env.MONGODB_PASSWORD,
-    username: process.env.MONGODB_USERNAME,
-  },
-  jwt: {
-    global: true,
-    secret: process.env.JWT_SECRET,
-    signOptions: {
-      expiresIn: process.env.JWT_EXPIRES_IN,
+export const config = () => {
+  const configObj = {
+    port: parseInt(process.env.PORT, 10) || 8080,
+    api: {
+      apiUrl: process.env.API_URL,
+      httpTimeout: 1000,
     },
-  },
-  s3: {
-    region: process.env.S3_REGION,
-    bucket: process.env.S3_BUCKET_NAME,
-    accessKeyId: process.env.S3_ACCESS_KEY_ID,
-    secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
-  },
-});
+    mongodb: {
+      uri: process.env.MONGODB_URI,
+      dbName: process.env.MONGODB_DBNAME,
+      password: process.env.MONGODB_PASSWORD,
+      username: process.env.MONGODB_USERNAME,
+    },
+    jwt: {
+      global: true,
+      secret: process.env.JWT_SECRET,
+      signOptions: {
+        expiresIn: process.env.JWT_EXPIRES_IN,
+      },
+    },
+    s3: {
+      region: process.env.S3_REGION,
+      bucket: process.env.S3_BUCKET_NAME,
+      accessKeyId: process.env.S3_ACCESS_KEY_ID,
+      secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
+    },
+  };
+
+  console.log('CONFIG OBJ', configObj);
+
+  return configObj;
+};
