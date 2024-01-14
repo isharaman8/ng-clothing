c4d37bf96a6a42afe331f56f8897ffc4cb33bb0d
Aman Kumar <isharaman8@gmail.com>
2024-01-14 12:49:09 +0530

    wip: adding fetch request to server for products

--- frontend/src/components/ListProducts/ListProducts.svelte
+++ frontend/src/components/ListProducts/ListProducts.svelte
@@ -1,20 +1,27 @@
 <script lang="ts">
 	// inner imports
-	import { sampleProducts, sampleTitle } from './ListProducts';
+	import { sampleProducts, sampleTitle, getProducts } from './ListProducts';
 	import ProductCard from '../ProductCard/ProductCard.svelte';
 
+	// props
 	export let title = sampleTitle;
-	export let products = sampleProducts;
+
+	export let params = {};
+
+	// defined variables
+	let _products = getProducts(params);
 </script>
 
-<section class="p-20">
-	<!-- title -->
-	<h1 class="text-3xl font-semibold">{title}</h1>
+{#await _products then products}
+	<section class="p-20">
+		<!-- title -->
+		<h1 class="text-3xl font-semibold">{title}</h1>
 
-	<!-- products -->
-	<div class="ml-5 grid grid-cols-4 gap-5 place-items-center place-content-center items-center content-center">
-		{#each products as product}
-			<ProductCard {product} />
-		{/each}
-	</div>
-</section>
+		<!-- products -->
+		<div class="ml-5 grid grid-cols-4 gap-5 place-items-center place-content-center items-center content-center">
+			{#each products.products as product}
+				<ProductCard {product} />
+			{/each}
+		</div>
+	</section>
+{/await}
