6ac1c3334a428792012e3d91e5a10235ad53f6d7
Aman Kumar <isharaman8@gmail.com>
2024-01-25 02:14:38 +0530

    wip: added cart schema

---
+++ backend/src/schemas/cart.schema.ts
@@ -0,0 +1,29 @@
+// inner imports
+import { HydratedDocument } from 'mongoose';
+
+// third party imports
+import { PurchaseProduct } from 'src/interfaces';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+
+export type CartDocument = HydratedDocument<Cart>;
+
+@Schema({ timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' } })
+export class Cart {
+  @Prop({ unique: true, required: true })
+  uid: string;
+
+  @Prop({ required: true })
+  user_id: string;
+
+  @Prop({ required: true, type: Array })
+  products: Array<PurchaseProduct>;
+}
+
+export const CartSchema = SchemaFactory.createForClass(Cart);
+
+// TODO
+/**
+ * fix PurchaseProduct -> add qty as well, change in controllers accordingly
+ * return total_price in purchase and cart request
+ * add cart controller and services
+ */
