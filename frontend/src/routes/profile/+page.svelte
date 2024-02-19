0d6d7f4723d685d9e5ae9a418e0317dbeeb1ebfc
Aman Kumar <isharaman8@gmail.com>
2024-02-19 09:17:45 +0530

    wip: added user profile route

---
+++ frontend/src/routes/profile/+page.svelte
@@ -0,0 +1,8 @@
+<script></script>
+
+<section class="mt-24 w-[100vw] min-h-[88vh] flex flex-col justify-start items-center">
+	<div class="w-[80%] mt-6">
+		<h1 class="text-4xl font text-gray-800 w-full">My Account</h1>
+		<div class="w-full min-h-[650px] bg-red-200 mt-8"></div>
+	</div>
+</section>
