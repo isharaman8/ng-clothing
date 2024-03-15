a412ae45cb52f86513a9e1dd894e19724eea17a8
Akshit Rana <akshitrana21@gmail.com>
2024-03-15 21:23:17 +0530

    added cart page and bug fixes

--- frontend/src/components/ProfileSections/UserOrders/EmptyOrderPage.svelte
+++ frontend/src/components/ProfileSections/UserOrders/EmptyOrderPage.svelte
@@ -1,13 +1,17 @@
-<script>
+<script lang="ts">
 	// third party imports
 	import { CartOutline } from 'flowbite-svelte-icons';
+
+	export let title: String;
+	export let description: String;
+	export let buttonName: String;
 </script>
 
 <div class="flex flex-col justify-center items-center gap-4 min-h-[400px]">
 	<CartOutline class="w-28 h-28 text-gray-600" strokeWidth={'1'} />
 	<div class="mt-3">
-		<h1 class="text-3xl text-gray-600 w-full text-center">No Orders Placed</h1>
-		<p class="w-full text-center text-gray-600 mt-3">Looks like you haven't made your order yet!</p>
+		<h1 class="text-3xl text-gray-600 w-full text-center">{title}</h1>
+		<p class="w-full text-center text-gray-600 mt-3">{description}</p>
 	</div>
-	<a href="/" class="px-3 py-4 rounded-lg bg-gray-700 text-white font-medium mt-8">Get Products</a>
+	<a href="/" class="px-3 py-4 rounded-lg bg-gray-700 text-white font-medium mt-8">{buttonName}</a>
 </div>
